8adad680bf32ba35f667b14b60d120d27a226e57
arturfdutra-boop <arturfdutra@gmail.com>
2026-08-19 12:58:50 +0000

    conclui o novato e aventureiro

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,50 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
-
 int main() {
     // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
+  // declarando a torre
+  // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
     // Exemplo: Crie uma função recursiva para o movimento do Bispo.
 
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
 
-    return 0;
+int torre = 1;
+while (torre <= 5)
+{
+printf ("direita\n");
+torre++;
 }
+
+// declarando a rainha 
+for (int rainha = 1; rainha <= 8; rainha++)
+printf ("esquerda\n");
+
+// declarando o bispo
+int bispo = 1;
+do {
+printf ("cima,direita\n");
+bispo++;    
+} while (bispo <= 5);
+    
+// nivel aventureiro - movimentar o cavalo
+// declarando o cavalo
+for(int cavalo = 1; cavalo <= 1; cavalo++){
+for(int cavalo = 1; cavalo <= 2; cavalo++){
+printf ( " cima\n %c\t", cavalo * cavalo);
+} 
+printf ("direita\n");
+}
+
+ 
+return 0;
+
+}
+  
+
+    
+   
+
