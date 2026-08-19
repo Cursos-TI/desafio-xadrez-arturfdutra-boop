82576b911e18ecf2a372b56d9f87979c6153db96
arturfdutra-boop <arturfdutra@gmail.com>
2026-08-19 14:37:32 +0000

    completei o mestre

--- xadrez.c
+++ xadrez.c
@@ -2,7 +2,7 @@
 
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
-int main() {
+
     // Nível Novato - Movimentação das Peças
   // declarando a torre
   // Nível Mestre - Funções Recursivas e Loops Aninhados
@@ -11,39 +11,58 @@ int main() {
 
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
-
-int torre = 1;
-while (torre <= 5)
-{
-printf ("direita\n");
-torre++;
+void movertorre(int torre) { 
+if (torre <= 0) {
+return;
 }
+printf (" movimento da torre -> direita\n");
+movertorre (torre - 1);
+}
+
 
 // declarando a rainha 
-for (int rainha = 1; rainha <= 8; rainha++)
-printf ("esquerda\n");
+void moverrainha(int rainha) {
+if (rainha <= 0) {
+  return;
+}
+printf ("movimento da rainha -> esquerda\n");
+moverrainha (rainha - 1);
+}
 
-// declarando o bispo
-int bispo = 1;
-do {
-printf ("cima,direita\n");
-bispo++;    
-} while (bispo <= 5);
-    
-// nivel aventureiro - movimentar o cavalo
+// declarando o bispo 
+void moverbispo (int bispo) {
+  if (bispo <= 0){
+    return;
+  }
+  printf("movimento do bispo -> direita\n");
+  printf ("movimento do bispo -> cima\n");
+  moverbispo ( bispo - 1);
+}
 // declarando o cavalo
-for(int cavalo = 1; cavalo <= 1; cavalo++){
-for(int cavalo = 1; cavalo <= 2; cavalo++){
-printf ( " cima\n %c\t", cavalo * cavalo);
-} 
-printf ("direita\n");
+void movercavalo (int cavalo) {
+if (cavalo == 0){
+ printf ("movimento do cavalo -> direita\n");
+  return;
+}
+ printf ("movimento do cavalo -> cima\n");
+
+movercavalo (cavalo - 1);
 }
 
- 
+int main () {
+
+int 
+totalcasas = 5;
+  movertorre(totalcasas);
+  totalcasas = 8;
+moverrainha(totalcasas);
+totalcasas = 5;
+moverbispo(totalcasas);
+totalcasas = 2;
+movercavalo(totalcasas);
 return 0;
-
 }
-  
+
 
     
    
